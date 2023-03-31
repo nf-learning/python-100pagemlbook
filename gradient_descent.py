c43225cf4f647cbe65e33bf392396f2623751254
Nigel Faria <nigel.faria@gmail.com>
2023-03-31 18:00:06 +0100

    feat: get_column_data from file

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,3 +1,17 @@
+import csv
+
+
+def get_column_data(filename, column_name):
+    with open(filename, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        header = next(reader)
+        column_index = header.index(column_name)
+        data = []
+        for row in reader:
+            data.append(float(row[column_index]))
+        return data
+
+
 def update_w_and_b(spendings, sales, w, b, alpha):
     dl_dw = 0.0
     dl_db = 0.0
@@ -5,7 +19,7 @@ def update_w_and_b(spendings, sales, w, b, alpha):
 
     for i in range(N):
         dl_dw += -2 * spendings[i] * (sales[i] - (w * spendings[i] + b))
-        dl_db += -2 * (sales[i](w * spendings[i] + b))
+        dl_db += -2 * (sales[i] - (w * spendings[i] + b))
 
     # update w and b
     w = w - (1 / float(N)) * dl_dw * alpha
@@ -29,3 +43,15 @@ def avg_loss(spendings, sales, w, b):
     for i in range(N):
         total_error = (sales[i] - (w * spendings[i] + b)) ** 2
     return total_error / float(N)
+
+
+def predict(x, w, b):
+    return w * x + b
+
+
+x = get_column_data('Advertising.csv', 'TV')
+y = get_column_data('Advertising.csv', 'sales')
+w, b = train(x, y, 0.0, 0.0, 0.001, 15000)
+x_new = 23.0
+y_new = predict(x_new, w, b)
+print(y_new)
